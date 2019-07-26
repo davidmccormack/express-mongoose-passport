4921d022673fa156a1e0500a80a4f1d3864f7c76
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2019-07-26 11:28:41 -0400

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userName: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: true
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema);
